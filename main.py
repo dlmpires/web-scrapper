283d78bc37de6568d429da487d389969016d5549
Diogo Pires <diogo.pires2211@gmail.com>
2024-08-26 18:40:25 +0100

    had to reorganize the file tree once more

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from actions import scrapeAll, scrapeKeywords
+from app.actions import scrapeAll, scrapeKeywords
 
 print("Hey :) \nWelcome to this simple web scrapper, for now, you have 2 options: \n\n1- Scrape the whole website\n2- Search keywords in quotes\n3- Search keywords in authors\n")
 choice = input("    >>> ")
